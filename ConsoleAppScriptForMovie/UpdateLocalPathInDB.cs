cc5db931bbd38309d7b05281dbcfce4296ebe378
cynosureofhyd2 <tomu646@gmail.com>
2013-12-19 22:03:52 -0700

    Clean add of ConsoleApp for Movie Recommendation

---
+++ ConsoleAppScriptForMovie/UpdateLocalPathInDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppScriptForMovie
+{
+    public class UpdateLocalPathInDB
+    {
+        public static void Update()
+        {
+            try
+            {
+                using (var db = new MovieEntities())
+                {
+                    string folderpath = @"C:\Users\PrashMaya\Pictures\MovieRecommendation\{0}";
+                    foreach (var poster in db.PosterInfoes)
+                    {
+                        var movieId = (from movie in db.Movies
+                                       where poster.MovieId == movie.ID
+                                       select movie.ImdbID).ToList();
+                        var localpath = string.Format(folderpath, movieId.FirstOrDefault());
+                        var imdbId = movieId.FirstOrDefault();
+                        poster.ImdbID = imdbId;
+                        db.SaveChanges();
+                    }   
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        
+    }
+}
